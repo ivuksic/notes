0d9bcc80b83154b47f2c284598a4f1252c00c7f4
Ivan Vuksic <ivuksic@gmail.com>
2024-01-27 20:44:36 +0100

    button functionality js

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ let notes = document.querySelectorAll(".input-box");
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
-  inputBox.className = "imput-box";
+  inputBox.className = "input-box";
   inputBox.setAttribute("contenteditable", "true");
   img.src = "images/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+  }
 });
